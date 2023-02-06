e28036d229ee2b3d67a8ca1e886ec2ca8a855425
Alireza Ahmadi <ahmadialireza317@gmail.com>
2023-02-07 00:56:58 +0330

    add db context class

---
+++ Infrastructure/DbContext/WishListManagementDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using Domain;
+
+namespace Infrastructure.DbContext
+{
+    public class WishListManagementDbContext : System.Data.Entity.DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<BaseEntity> BaseEntities { get; set; }
+        public DbSet<WishList> WishLists { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.AddFromAssembly(typeof(WishListManagementDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
