535349e0c3678b48ab6e106e5d3aa5dd31b4fad3
Alireza Ahmadi <ahmadialireza317@gmail.com>
2023-02-07 00:33:27 +0330

    add domain and entity classes

---
+++ Domain/User/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class User : BaseEntity
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public List<WishList> WishLists { get; set; }
+
+    }
+}
