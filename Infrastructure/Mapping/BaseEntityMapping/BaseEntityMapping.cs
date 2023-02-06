7abc92c66e022c076da0a67ed59393cf12860e18
Alireza Ahmadi <ahmadialireza317@gmail.com>
2023-02-07 00:45:47 +0330

    set mapping for classes

---
+++ Infrastructure/Mapping/BaseEntityMapping/BaseEntityMapping.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+
+namespace Infrastructure.Mapping.BaseEntityMapping
+{
+    public class BaseEntityMapping : EntityTypeConfiguration<BaseEntity>
+    {
+        public BaseEntityMapping()
+        {
+            HasKey(a => a.Id);
+            Property(a => a.CreatedDateTime).IsRequired();
+            Property(a => a.ModifiedDateTime).IsRequired();
+        }
+    }
+}
---
+++ Infrastructure/Mapping/WishListMapping/WishListMapping.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Infrastructure.Mapping
+{
+    public class WishListMapping : EntityTypeConfiguration<WishList>
+    {
+        public WishListMapping()
+        {
+            Property(a => a.WishlistDescription).IsRequired();
+            Property(a => a.Priority).IsOptional();
+            Property(a => a.RoughPrice).IsOptional();
+        }
+    }
+}
