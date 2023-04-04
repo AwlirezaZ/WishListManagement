8a9a6a4bfe5ff83199c9742e0982888985d3fc23
a.ahmadi <a.ahmadi@papgroup>
2023-04-04 13:02:39 +0430

    set authorize annotation on user controller

--- WishlistManagement/Controllers/UserController.cs
+++ WishlistManagement/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WishListManagement.Core.DbContext;
 using WishListManagement.Models.ViewModels.User;
 using WishListManagement.Services;
 
@@ -32,17 +33,23 @@ namespace WishListManagement.Controllers
             var id = _userService.Create(user);
             return RedirectToAction("UserInfo",new {id});
         }
-
+        [Authorize]
         public ActionResult UserInfo(long id)
         {
             var userViewModel = _userService.GetUserById(id);
+            if (userViewModel.Username != HttpContext.User.Identity.Name) 
+                throw new UnauthorizedAccessException("we are not allowed to show other users' information to you");
             return View(userViewModel);
         }
+        [Authorize]
         public ActionResult EditUser(long id)
         {
             var userViewModel = _userService.GetUserById(id);
+            if (userViewModel.Username != HttpContext.User.Identity.Name)
+                throw new UnauthorizedAccessException("you are not allowed to show other users' information");
             return View(userViewModel);
         }
+        [Authorize]
         [HttpPost]
         public ActionResult EditUser(ModifyUserViewModel user)
         {
